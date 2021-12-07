4e8dc57ac9e256965ab03784e43128f04aac6fec
Luifer1994 <almendralesluifer@gamil.com>
2021-12-07 00:08:49 -0500

    return name user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('content')
-<h2 class="mt-5">Bienvenido al sistema</h2>
+<h3 class="mt-5">Bienvenido al sistema: <span class="text-primary">{{Auth::user()->name}}</span></h3>
 <form action="{{route('logout')}}" method="POST">
     @csrf
     <button class="btn btn-danger">Cerrar sesión</button>
